f3bb82ff18d17a20609a824b29fc6745a653de42
StéphaneNOTTER <s.notter@sentelis.com>
2020-12-10 10:10:25 +0100

    add date for SEPATransaction

--- src/main/java/org/java/sepaxml/examples/ExampleCreditTransferMarketPay.java
+++ src/main/java/org/java/sepaxml/examples/ExampleCreditTransferMarketPay.java
@@ -6,7 +6,10 @@ import org.java.sepaxml.SEPACreditTransfer;
 import org.java.sepaxml.SEPATransaction;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ExampleCreditTransferMarketPay {
@@ -14,29 +17,41 @@ public class ExampleCreditTransferMarketPay {
         final SEPABankAccount sender = new SEPABankAccount(
                 "DE89370400440532013000",
                 "DEUTDEBBXXX",
-                "Joe Doe"
+                "MARKET PAY"
         );
 
-
         final List<SEPATransaction> transactions = new ArrayList<SEPATransaction>() {{
-            add(new SEPATransaction(
-                            new SEPABankAccount(
-                                    "ES8600491500052610115791",
-                                    "BSCHESMMXXX",
-                                    "C.C. CARREFOUR S.A."
-                            ),
-                            // new BigDecimal(18544082.14d),
-                            BigDecimal.valueOf(18544082.14),
-                            "07.12.2020 BRUTO: 18580731,32 COM: 36649,18",
-                            SEPATransaction.Currency.EUR
-                    )
-            );
+            try {
+                Date dt;
+                dt = new SimpleDateFormat("yyyy-MM-dd").parse("2020-12-07");
+
+                add(new SEPATransaction(
+                        new SEPABankAccount(
+                                "ES8600491500052610115791",
+                                "BSCHESMMXXX",
+                                "C.C. CARREFOUR S.A."
+                        ),
+                        // new BigDecimal(18544082.14d),
+                        BigDecimal.valueOf(18544082.14),
+                        "07.12.2020 BRUTO: 18580731,32 COM: 36649,18",
+                        dt,
+                        SEPATransaction.Currency.EUR)
+                );
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }};
 
-        final SEPA sepa = new SEPACreditTransfer(sender, transactions);
-        System.out.println("Market Pay");
-        sepa.write(System.out);
 
+        // final SEPA sepa = new SEPACreditTransfer(sender, transactions);
+        try {
+            final SEPA sepa = new SEPACreditTransfer(sender, transactions, new SimpleDateFormat("yyyy-MM-ddhh:mm:ss").parse("2020-12-0708:21:05"));
+            System.out.println("Market Pay");
+            sepa.write(System.out);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
